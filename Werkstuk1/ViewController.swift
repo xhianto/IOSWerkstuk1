80e8a26a30548ad8a1783106e40f8317a463c631
Ka Hea Lam <xhianto@gmail.com>
2021-05-21 01:44:56 -0700

    Initial Commit

---
+++ Werkstuk1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Werkstuk1
+//
+//  Created by student on 21/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
