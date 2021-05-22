b97d8a460fe83be8d9dbc87d063a9e114c6f429c
Ka Hea Lam <xhianto@gmail.com>
2021-05-22 09:45:52 -0700

    Added van nieuwe vaccinatie centrum + automatisch coordinaten

--- Werkstuk1/CentrumToevoegenVC.swift
+++ Werkstuk1/CentrumToevoegenVC.swift
@@ -6,12 +6,93 @@
 //
 
 import UIKit
-import CoreLocation
 import MapKit
+import CoreLocation
 
 class CentrumToevoegenVC: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var InputNaam: UITextField!
+    @IBOutlet weak var InputStraat: UITextField!
+    @IBOutlet weak var InputHuisnummer: UITextField!
+    @IBOutlet weak var InputPostcode: UITextField!
+    @IBOutlet weak var InputGemeente: UITextField!
+    @IBOutlet weak var InputTelefoonnummer: UITextField!
+    @IBOutlet weak var ButtonSave: UIBarButtonItem!
+    @IBOutlet weak var LabelWaarschuwing: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        InputNaam.layer.borderColor = UIColor.red.cgColor
+        InputStraat.layer.borderColor = UIColor.red.cgColor
+        InputHuisnummer.layer.borderColor = UIColor.red.cgColor
+        InputPostcode.layer.borderColor = UIColor.red.cgColor
+        InputGemeente.layer.borderColor = UIColor.red.cgColor
+        InputTelefoonnummer.layer.borderColor = UIColor.red.cgColor
     }
+    
+    @IBAction func Action(_ sender: Any) {
+        if AllesIngevuld() {
+            let vCentrum = VaccinatieCentrum()
+            vCentrum.title = InputNaam.text
+            vCentrum.image = "Hospital"
+            vCentrum.address.street = InputStraat.text!
+            vCentrum.address.number = InputHuisnummer.text!
+            vCentrum.address.postalCode = InputPostcode.text!
+            vCentrum.address.city = InputGemeente.text!
+            vCentrum.phone = InputTelefoonnummer.text!
+            
+            let geocoder = CLGeocoder()
+            geocoder.geocodeAddressString("\(InputStraat.text!) \(InputHuisnummer.text!) \(InputGemeente.text!) \(InputPostcode.text!)") {
+                placemarks, error in
+                let placemark = placemarks?.first
+                let lat = placemark?.location?.coordinate.latitude
+                let lon = placemark?.location?.coordinate.longitude
+                vCentrum.coordinate = CLLocationCoordinate2D(latitude: lat ?? 0, longitude: lon ?? 0)
+            }
+            //vCentrum.coordinate = CLLocationCoordinate2D(latitude: 50.84324049747469, longitude: 4.332446384072508)
+            LijstCentrum.shared.lijst.append(vCentrum)
+            self.navigationController?.popViewController(animated: true)
+        }
+        else {
+            LabelWaarschuwing.alpha = 1
+        }
+    }
+    
+    func AllesIngevuld() -> Bool {
+        var ingevuld = true
+        InputNaam.layer.borderWidth = 0
+        InputStraat.layer.borderWidth = 0
+        InputHuisnummer.layer.borderWidth = 0
+        InputPostcode.layer.borderWidth = 0
+        InputGemeente.layer.borderWidth = 0
+        InputTelefoonnummer.layer.borderWidth = 0
+        LabelWaarschuwing.alpha = 0
+        if InputNaam.text == "" {
+            InputNaam.layer.borderWidth = 1
+            ingevuld = false
+        }
+        if InputStraat.text == "" {
+            InputStraat.layer.borderWidth = 1
+            ingevuld = false
+        }
+        if InputHuisnummer.text == "" {
+            InputHuisnummer.layer.borderWidth = 1
+            ingevuld = false
+        }
+        if InputPostcode.text == "" {
+            InputPostcode.layer.borderWidth = 1
+            ingevuld = false
+        }
+        if InputGemeente.text == "" {
+            InputGemeente.layer.borderWidth = 1
+            ingevuld = false
+        }
+        if InputTelefoonnummer.text == "" {
+            InputTelefoonnummer.layer.borderWidth = 1
+            ingevuld = false
+        }
+        return ingevuld
+    }
+    
 }
 
--- Werkstuk1/LijstCentrumsVC.swift
+++ Werkstuk1/LijstCentrumsVC.swift
@@ -12,12 +12,13 @@ class LijstCentrumsVC: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.reloadData()
         // Do any additional setup after loading the view.
         //LijstCentrum.shared.lijst
-        let address5 = Address(street: "Pachecolaan", number: "42", postalCode: "1000", city: "Tester")
-        let centrum5 = VaccinatieCentrum(title: "Tester", image: "Hospital", address: address5, coordinate: CLLocationCoordinate2D(latitude: 50.84324049747469, longitude: 4.332446384072508), phone: "")
+        //let address5 = Address(street: "Pachecolaan", number: "42", postalCode: "1000", city: "Tester")
+        //let centrum5 = VaccinatieCentrum(title: "Tester", image: "Hospital", address: address5, coordinate: CLLocationCoordinate2D(latitude: 50.84324049747469, longitude: 4.332446384072508), phone: "")
         
-        LijstCentrum.shared.lijst.append(centrum5)
+        //LijstCentrum.shared.lijst.append(centrum5)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -30,10 +31,16 @@ class LijstCentrumsVC: UITableViewController {
         cell.detailTextLabel?.text = LijstCentrum.shared.lijst[indexPath.row].address.city
         cell.imageView?.image = UIImage(named: LijstCentrum.shared.lijst[indexPath.row].image)
         cell.imageView?.contentMode = .scaleToFill
-        
         return cell
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        self.tableView.reloadData()
+    }
+    
+    /*
+     Voor delete: http://www.youtube.com/watch?v=5QdAPoFYBqM
+     */
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             LijstCentrum.shared.lijst.remove(at: indexPath.row)
